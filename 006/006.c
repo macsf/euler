40115504ba508b2921961a653e79112de41030f8
Sutthichok Wongthavorn <sutthichok@gmail.com>
2012-04-29 01:50:48 +0700

    Problem 006. Completed.

---
+++ 006/006.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sqr(int);
+
+int main() {
+  const int LIMIT = 100;
+  unsigned int i,
+    sum = 0,
+    sum_sqr = 0,
+    sqr_sum = 0,
+    diff = 0;
+
+  for (i = 1; i <= LIMIT; i++) {
+    sum += i;
+    sum_sqr += sqr(i);
+  }
+  sqr_sum = sqr(sum);
+
+  printf("sum of square = %d\n", sum_sqr);
+  printf("square of sum = %d\n", sqr_sum);
+  printf("diff = %d", abs(sum_sqr - sqr_sum));
+
+  return 0;
+}
+
+int sqr(int x) {
+  return x * x;
+}
