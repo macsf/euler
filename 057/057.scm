7e65a188b031e19fe0f09287cb521adb66b49e59
Sutthichok Wongthavorn <sutthichok@gmail.com>
2012-05-24 14:33:44 +0700

    Include the problem information.

--- 057/057.scm
+++ 057/057.scm
@@ -1,3 +1,12 @@
+;; Problem 57 (21 November 2003)
+;;
+;; Investigate the expansion of the continued fraction for the square root
+;; of two.
+;; ---
+;; Answer:
+;; 153
+;;
+
 (define (sqrte x times)
   (define (iter expansion)
     (if (= expansion 0)
@@ -6,20 +15,20 @@
   (+ 1 (iter times)))
 
 (define (euler57 n max)
-  
+
   (define (iter times count)
     (define sq 0)
-    
+
     (if (> times max)
         count
         (begin
           (set! sq (sqrte n times))
-          (if (> 
+          (if (>
                (string-length (number->string (numerator sq)))
                (string-length (number->string (denominator sq))))
               (set! count (+ 1 count)))
           (iter (+ 1 times) count))))
-  
+
   (iter 0 0))
 
-(display (euler57 2 1000))
+;; (display (euler57 2 1000))
