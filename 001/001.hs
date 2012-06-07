f953a259e1ad23dad29dd8eb0df7683ff70ccf7e
Sutthichok Wongthavorn <sutthichok@gmail.com>
2012-06-07 09:44:25 +0700

    can run directly on command-line.

--- 001/001.hs
+++ 001/001.hs
@@ -1,1 +1,7 @@
-sum35 = sum [ x | x <- [1..1000-1], x `mod` 3 == 0 || x `mod` 5 == 0 ]
+-- > runhaskell 001.hs
+-- or compile with ghc
+
+main = do
+  print euler001
+
+euler001 = sum [ x | x <- [1..1000-1], x `mod` 3 == 0 || x `mod` 5 == 0 ]
