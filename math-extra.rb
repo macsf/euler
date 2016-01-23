b7b7354e965c48d831a661f454f17cb86a316602
Sutthichok Wongthavorn <sutthichok@gmail.com>
2016-01-23 20:57:16 +0700

    factors.

--- math-extra.rb
+++ math-extra.rb
@@ -16,7 +16,22 @@ def list_fibs(max)
 
   fib_nums
 end
+
+require 'prime'
+
 class Integer
+  def factors
+    return nil if self < 2
+
+    primes, powers = prime_division.transpose
+    exponents = powers.map { |i| (0..i).to_a }
+    divisors = exponents.shift.product(*exponents).map do |pows|
+      primes.zip(pows).map { |prime, power| prime**power }.inject(:*)
+    end
+
+    divisors.sort.map { |div| [div, self / div] }
+  end
+
   def prime_factorization(n)
     def factor_generator(n)
       for prime_candidate in Prime::Generator23.new
